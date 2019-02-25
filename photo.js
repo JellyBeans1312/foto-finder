95ccf03185a67e7b11702d390c64aa823f7fdd8f
Aidan McKay <aidanmckay2000@gmail.com>
2019-02-25 08:05:02 -0700

    Setting the basics in for the javascript functionality

---
+++ photo.js
@@ -0,0 +1,17 @@
+class Photo {
+  constructor(id, title, body, favorite) {
+  this.id = id;
+  this.title = title;
+  this.body = body;
+  this.favorite = favorite;
+  }
+  // saveToStorage() {
+    
+  // }
+  // deleteFromStorage() {
+
+  // }
+  // updatePhoto() {
+
+  // }
+}
