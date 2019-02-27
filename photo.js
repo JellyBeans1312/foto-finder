37ac0e3cc46e863d760d8ce7e9f08b69f3307c7b
Aidan McKay <aidanmckay2000@gmail.com>
2019-02-26 21:30:10 -0700

    changing the semantics on my html page

--- photo.js
+++ photo.js
@@ -1,17 +1,19 @@
 class Photo {
-  constructor(id, title, body, favorite) {
+  constructor(id, title, image, body, favorite = false) {
   this.id = id;
   this.title = title;
   this.body = body;
   this.favorite = favorite;
   }
-  // saveToStorage() {
-    
-  // }
-  // deleteFromStorage() {
+  saveToStorage(newArr) {
+    var stringPhotos = JSON.stringify(newArr);
+    localStorage.setItem('stringifiedPhotos', stringPhotos);
+  }
+  deleteFromStorage(index) {
+    allPhtoos.splice(index, 1);
+    this.saveToStorage(allPhotos);
+  }
+  updatePhoto() {
 
-  // }
-  // updatePhoto() {
-
-  // }
+  }
 }
